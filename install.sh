e743ede7b488a56924c6342b28e5612fa6a10756
Risto Kotalampi <risto@texas.kotalampi.com>
2016-07-17 04:10:50 +0000

    adding two files

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ cd /usr/local/; git clone https://github.com/rkotalampi/HCDX-Online-Log.git
 service mariadb start
 
 cd /usr/local/HCDX-Online-Log/; mysql HCDX -p < init_db.sql
-cat /usr/local/HCDX-Online-Log/online-log.conf| sed -e s/DNS/$DNS/g | sed -e s/ADMIN/$ADMIN/g | sed -e s/ROOT/$ROOT/g > /etc/httpd/conf.d/online-log.conf
+cat /usr/local/HCDX-Online-Log/online-log.conf| sed -e "s/DNS/$DNS/g" | sed -e "s/ADMIN/$ADMIN/g" | sed -e "s/ROOT/$ROOT/g" > /etc/httpd/conf.d/online-log.conf
 service httpd restart
 
 
